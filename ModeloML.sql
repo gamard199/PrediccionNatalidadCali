28a7b94e23c1c904512ed9ff3842c39c21b10b8d
Geovanny <birdcode.py@gmail.com>
2024-11-19 10:01:44 -0500

    Creacion de campos de numero de mujeres en edad reproductiva retroactivo a 15 años antes del Censo 2018

--- ModeloML.sql
+++ ModeloML.sql
@@ -1,1 +1,10 @@
+-- Creacion de extension Postgis para el soporte de datos espaciales en la base de datos
+
 CREATE EXTENSION postgis;
+
+-- Creacion de los campos de numero de mujeres en edad reproductiva MER para los
+-- periodos 2013 a 2017, 2008 a 2013 y 2003 a 2007 (15 años antes del Censo 2018)
+
+ALTER TABLE manzanas_cali ADD COLUMN mer13_17 int, ADD COLUMN mer08_13 int,
+ADD COLUMN mer03_07 int;
+
