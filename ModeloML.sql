f014cfae2988dbf3eddd2538f29d98e0abc0e241
Geovanny <birdcode.py@gmail.com>
2024-11-19 12:49:04 -0500

    Creacion de campos para los indicadores de porcentaje de nacimientos para cada año respecto al numero de mujeres en edad reproductiva

--- ModeloML.sql
+++ ModeloML.sql
@@ -15,3 +15,18 @@ UPDATE manzanas_cali SET mer13_17 = (mujr20_24 + mujr25_29 + mujr30_34 + mujr35_
 UPDATE manzanas_cali SET mer08_12 = (mujr25_29 + mujr30_34 + mujr35_39 + mujr40_44 + mujr45_49 + mujr50_54); 
 UPDATE manzanas_cali SET mer03_07 = (mujr30_34 + mujr35_39 + mujr40_44 + mujr45_49 + mujr50_54 + mujr55_59); 
 
+-- Calculo de indicadores de porcentaje de nacimientos con respecto al total de
+-- mujeres en edad reproductiva para cada año desde 2003 hasta 2017
+
+---- Creacion de campos para indicadores
+
+ALTER TABLE manzanas_cali ADD COLUMN prc_nmer03 double precision,
+ADD COLUMN prc_nmer04 double precision, ADD COLUMN prc_nmer05 double precision,
+ADD COLUMN prc_nmer06 double precision, ADD COLUMN prc_nmer07 double precision,
+ADD COLUMN prc_nmer08 double precision, ADD COLUMN prc_nmer09 double precision,
+ADD COLUMN prc_nmer10 double precision, ADD COLUMN prc_nmer11 double precision,
+ADD COLUMN prc_nmer12 double precision, ADD COLUMN prc_nmer13 double precision,
+ADD COLUMN prc_nmer14 double precision, ADD COLUMN prc_nmer15 double precision;
+
+
+
